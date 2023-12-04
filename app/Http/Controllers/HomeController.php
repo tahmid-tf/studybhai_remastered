44e41a7d7eae41e30644195bccd5d70c5d7c86e6
tahmid <tahmid.tf1@gmail.com>
2023-12-04 14:14:18 +0600

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,6 +28,6 @@ class HomeController extends Controller
             return redirect()->route('course.index');
         }
 
-        return view('home');
+        return redirect()->route('/');
     }
 }
